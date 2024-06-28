78ce8bbca7a6be5f443d50fe0c7272242b6461c5
kaggy <kaggy8@ya.ru>
2024-06-29 00:44:12 +0300

    Refactoring tests

--- src/test/java/com/example/bing/tests/BingSearchTest.java
+++ src/test/java/com/example/bing/tests/BingSearchTest.java
@@ -8,24 +8,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
 public class BingSearchTest {
+    public static  final String INPUT = "Selenium";
     private WebDriver driver;
+    private MainPage mainPage;
+    private ResultsPage resultsPage;
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.bing.com");
-
+        mainPage = new MainPage(driver);
+        resultsPage = new ResultsPage(driver);
     }
 
     @AfterEach
@@ -36,31 +36,22 @@ public class BingSearchTest {
     @Test
     @DisplayName("Search in Bing")
     public void searchFieldTest() {
-        String input = "Selenium";
-        MainPage mainPage = new MainPage(driver);
-        mainPage.sendText(input);
-
-        ResultsPage resultsPage = new ResultsPage(driver);
-
-        assertEquals(input, resultsPage.getTextFromSearchField(), "Текст не совпал");
+        mainPage.sendText(INPUT);
+        assertEquals(INPUT, resultsPage.getTextFromSearchField(), "Текст не совпал");
     }
 
     @Test
     @DisplayName("Click on Selenium")
     public void searchResultTest() {
-        String input = "Selenium";
         String currentLink = "https://www.selenium.dev/";
         int indexPage = 0;
 
-        MainPage mainPage = new MainPage(driver);
-        mainPage.sendText(input);
-
-        ResultsPage resultsPage = new ResultsPage(driver);
+        mainPage.sendText(INPUT);
         resultsPage.waitForLink();
         resultsPage.clickOnIndex(indexPage);
         resultsPage.switchWindow();
 
         String seleniumMainPage = driver.getCurrentUrl();
-        assertEquals(currentLink, seleniumMainPage, "Открылась не верная ссылка");
+        assertEquals(currentLink, seleniumMainPage, "Открылась неверная ссылка");
     }
 }
